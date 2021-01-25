0a5af50f311aa093fa8191a66a6e22bac7e67cfc
Aakanksha <77713516+aknsha@users.noreply.github.com>
2021-01-25 13:27:51 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,71 @@
+<?php include 'header.php';?>
+
+<body>
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v9.0" nonce="iGYBU2wl"></script>
+<section class="about-banner"></section>    
+<!--Menu Bar--> 
+<section class="header">
+    <div class="container">
+        <div class="col-12 col-lg-11 d-none d-lg-flex mx-auto header-top">
+            <div class="social-icons">
+                <a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a>
+				<a href="https://www.facebook.com"><i class="fa fa-instagram"></i></a>
+                <a href="https://www.twitter.com/osom_rummy/"><i class="fa fa-twitter"></i></a>
+                <a href="https://www.facebook.com"><i class="fa fa-google"></i></a>
+            </div>
+            <div class="header-top-content">
+                <a href="#">&#128383; &nbsp;Support</a>
+                <a href="responsible.php">&#10070; &nbsp;Responsible Gaming</a>
+            </div>
+        </div>
+        <nav class="navbar col-12 col-lg-11 mx-auto navbar-expand-lg navbar-dark">
+            <div class="container-fluid">
+              <a href="index.php"><img src="images/logo2.png"></a>
+              <button class="navbar-toggler collapsed navbar-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="about.php">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="privacy.php">Privacy Policy</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="refund.php">Refund Policy</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="privacy.php">Products & Services</a>
+                </li>      
+                </ul>
+            </div>
+            <img class="d-none d-lg-block quote" src="images/get_quote.png">
+        </div>
+      </nav>
+</section>
+<!-- Menu End-->
+
+    <section class="content">
+        <div class="col-11 col-lg-10 mx-auto">
+            <p>
+                <b>RUBIK CUBE TECHNOLOGY INDIA PRIVATE LIMITED Description</b>
+    Get the best and easiest rummy experience in our App, India’s favourite casino game! Play rummy for free and join cash tables to win ultimate cash prizes. Enjoy the fun and excitement of playing rummy on the go, anywhere, anytime. And not just rummy, it has fantasy cricket too!
+    
+    <b>RUBIK CUBE TECHNOLOGY INDIA PRIVATE LIMITED</b> has never-before-seen features making your rummy play super easy and your game much more thrilling and enjoyable. Play with your friends, family or other players all over the country and win real cash quick.
+    
+    <b>RUBIK CUBE TECHNOLOGY INDIA PRIVATE LIMITED</b> brings to you a whole new era of rummy games
+
+            </p>
+        </div>
+    </section>
+	
+<!-- FOOTER START -->
+<?php include 'footer.php';?>
